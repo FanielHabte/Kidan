9f0114dd43def468fd0f12ceb8870df04dfde54d
Fani <fanielhabte20@gmail.com>
2026-04-19 23:06:30 -0700

    Initial Commit

---
+++ src/main/java/io/kidan/KidanApplication.java
@@ -0,0 +1,13 @@
+package io.kidan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/kidan/KidanApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kidan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
